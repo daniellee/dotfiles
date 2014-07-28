0b861862c66b15c564de7c7b4ebfbb7fe4278095
Torkel Ödegaard <torkel.odegaard@gmail.com>
2014-07-28 12:27:07 +0200

    fixed fonts issue on desktop comp

--- sublime/linux.sh
+++ sublime/linux.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")"
 
 echo "Installing Sublime Text Link"
 
-ln --symbolic "$HOME/.dotfiles/sublime/User" "$HOME/.config/sublime-text-3/Packages/User"
+# ln --symbolic "$HOME/.dotfiles/sublime/User" "$HOME/.config/sublime-text-3/Packages/User"
