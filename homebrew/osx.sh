c179bfa7acd30cecba8ca5540e725e4b823cd0d4
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-01-08 21:22:30 +0100

    Mac OSX additions

---
+++ homebrew/osx.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+set -e
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+cd "$(dirname "$0")"
+
+# homebrew packages
+
+TAPS=(
+    homebrew/binary
+    homebrew/dupes
+    homebrew/versions
+    caskroom/cask
+    caskroom/versions
+)
+
+FORMULAS=(
+    caskroom/cask/brew-cask
+    coreutils
+    ctags
+    erlang
+    go
+)
+
+CASKS=(
+    firefox
+    p4merge
+    skype
+    spotify
+    sublime-text3
+    the-unarchiver
+    utorrent
+    virtualbox
+    visualvm
+    vlc
+    xquartz
+)
+
+for tap in ${TAPS[@]}
+do
+    brew tap $tap
+done
+
+brew install ${FORMULAS[@]}
+
+brew update
+
+brew cask install ${CASKS[@]}
+
+# Special cases that need flags...
+brew install macvim --with-cscope --with-lua --HEAD
+brew install vim --with-lua
+brew linkapps macvim
+
+brew cleanup
+
+exit 0
